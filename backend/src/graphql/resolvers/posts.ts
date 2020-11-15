6e572dec2c900381faa192a68dfb07e19befd64c
Ion Cretu <cretuion94@gmail.com>
2020-11-15 19:55:23 +0200

    husky setup

--- backend/src/graphql/resolvers/posts.ts
+++ backend/src/graphql/resolvers/posts.ts
@@ -36,7 +36,7 @@ const postsResolver = {
 			}
 			const newPost = new Post({
 				body,
-				user: user...,
+				user: user.id,
 				username: user.username,
 				createdAt: new Date().toISOString(),
 			});
